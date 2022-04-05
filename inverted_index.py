9ba0167fa6f8df4163de70a81d22c30e10650b6c
sumbel <s.enikeeva1@mail.ru>
2022-04-06 00:12:14 +0300

    doc tokens arg

--- inverted_index.py
+++ inverted_index.py
@@ -3,20 +3,25 @@ import os
 from preprocessing import get_tokens
 
 
-def inverted_index_map(data_folder):
+def inverted_index_map(data_folder, get_doc_tokens=False):
     files = os.listdir(data_folder)
     _inverted_index_map = {}
+    doc_tokens = {}
     for f_ in files:
         data = ''
         with open(f"{data_folder}/{f_}", 'r') as f:
             data += f.read()
             tokens = get_tokens(data)
+            doc_tokens[int(f_.split('.')[0])] = tokens
             for token in tokens:
                 if _inverted_index_map.get(token) is None:
                     _inverted_index_map[token] = [int(f_.split('.')[0])]
                 else:
                     _inverted_index_map[token].append(int(f_.split('.')[0]))
-    return _inverted_index_map
+    if get_doc_tokens:
+        return _inverted_index_map, doc_tokens
+    else:
+        return _inverted_index_map
 
 
 def main():
